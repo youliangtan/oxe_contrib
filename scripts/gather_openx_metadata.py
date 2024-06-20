0f18003a766852ff8686e0293efaf0fa8a30f1c2
Kaiyuan Eric Chen <kych@berkeley.edu>
2024-06-19 23:11:35 -0700

    add more information of datasets from google sheet

--- scripts/gather_openx_metadata.py
+++ scripts/gather_openx_metadata.py
@@ -12,7 +12,7 @@ class ConfigSchema(BaseModel):
     link: str
     curation: List[dict]
     schema: List[dict]
-    intended_level_of_support: int
+    level_of_support: int
     copyright: str
     number_of_trajectories: List[dict]
     size_in_gb: float
@@ -31,16 +31,16 @@ class ConfigSchema(BaseModel):
             raise ValueError("feature_size must be an integer")
         return value
 
-    @validator('intended_level_of_support', "number_of_trajectories")
-    def validate_intended_level_of_support(cls, value):
+    @validator('level_of_support', "number_of_trajectories")
+    def validate_level_of_support(cls, value):
         if not isinstance(value, int):
-            raise ValueError("intended_level_of_support must be an integer")
+            raise ValueError("level_of_support must be an integer")
         return value
 
     @validator('size_in_gb')
-    def validate_intended_level_of_support(cls, value):
+    def validate_level_of_support(cls, value):
         if not isinstance(value, float):
-            raise ValueError("intended_level_of_support must be an integer")
+            raise ValueError("level_of_support must be an integer")
         return value
     
     @validator('tag', each_item=True)
@@ -53,7 +53,7 @@ class ConfigSchema(BaseModel):
 class Config:
     dataset_name: str
     description: str
-    intended_level_of_support: int 
+    level_of_support: int 
     copyright: str 
     size_in_gb: float
     version: str 
@@ -81,62 +81,6 @@ class Config:
 import numpy as np
 import tensorflow_datasets as tfds
 
-DATASETS = [
-    'fractal20220817_data',
-    'kuka',
-    'bridge',
-    'taco_play',
-    'jaco_play',
-    'berkeley_cable_routing',
-    'roboturk',
-    'nyu_door_opening_surprising_effectiveness',
-    'viola',
-    'berkeley_autolab_ur5',
-    'toto',
-    'language_table',
-    'columbia_cairlab_pusht_real',
-    'stanford_kuka_multimodal_dataset_converted_externally_to_rlds',
-    'nyu_rot_dataset_converted_externally_to_rlds',
-    'stanford_hydra_dataset_converted_externally_to_rlds',
-    'austin_buds_dataset_converted_externally_to_rlds',
-    'nyu_franka_play_dataset_converted_externally_to_rlds',
-    'maniskill_dataset_converted_externally_to_rlds',
-    'cmu_franka_exploration_dataset_converted_externally_to_rlds',
-    'ucsd_kitchen_dataset_converted_externally_to_rlds',
-    'ucsd_pick_and_place_dataset_converted_externally_to_rlds',
-    'austin_sailor_dataset_converted_externally_to_rlds',
-    'austin_sirius_dataset_converted_externally_to_rlds',
-    'bc_z',
-    'usc_cloth_sim_converted_externally_to_rlds',
-    'utokyo_pr2_opening_fridge_converted_externally_to_rlds',
-    'utokyo_pr2_tabletop_manipulation_converted_externally_to_rlds',
-    'utokyo_saytap_converted_externally_to_rlds',
-    'utokyo_xarm_pick_and_place_converted_externally_to_rlds',
-    'utokyo_xarm_bimanual_converted_externally_to_rlds',
-    'robo_net',
-    'berkeley_mvp_converted_externally_to_rlds',
-    'berkeley_rpt_converted_externally_to_rlds',
-    'kaist_nonprehensile_converted_externally_to_rlds',
-    'stanford_mask_vit_converted_externally_to_rlds',
-    'tokyo_u_lsmo_converted_externally_to_rlds',
-    'dlr_sara_pour_converted_externally_to_rlds',
-    'dlr_sara_grid_clamp_converted_externally_to_rlds',
-    'dlr_edan_shared_control_converted_externally_to_rlds',
-    'asu_table_top_converted_externally_to_rlds',
-    'stanford_robocook_converted_externally_to_rlds',
-    'eth_agent_affordances',
-    'imperialcollege_sawyer_wrist_cam',
-    'iamlab_cmu_pickup_insert_converted_externally_to_rlds',
-    'uiuc_d3field',
-    'utaustin_mutex',
-    'berkeley_fanuc_manipulation',
-    'cmu_play_fusion',
-    'cmu_stretch',
-    'berkeley_gnm_recon',
-    'berkeley_gnm_cory_hall',
-    'berkeley_gnm_sac_son'
-]
-
 def dataset2path(dataset_name):
   if dataset_name == 'robo_net':
     version = '1.0.0'
@@ -146,45 +90,64 @@ def dataset2path(dataset_name):
     version = '0.1.0'
   return f'gs://gresearch/robotics/{dataset_name}/{version}'
 
-dataset = 'fractal20220817_data'
-b = tfds.builder_from_directory(builder_dir=dataset2path(dataset))
+# read csv "scripts/Open X-Embodiment Dataset Overview - Dataset Overview.csv"
+import pandas as pd
+with open("scripts/Open X-Embodiment Dataset Overview - Dataset Overview.csv") as f:
+    # remove the first 15 lines and read as csv
+    df = pd.read_csv(f, skiprows=14)
+    print(df.columns)
 
-# Get the dataset info
-info = b.info
-print(info)
-
-def from_open_x_to_config(
-    ds_info: tfds.core.DatasetInfo,
-):
+# iterate over df
+for i, row in df.iterrows():
+    dataset_name = row["Registered Dataset Name"]
+    try:
+        ds_info = tfds.builder_from_directory(builder_dir=dataset2path(dataset_name)).info
+        version = str(ds_info.version)
+        download = [{"source": "google_bucket", "link": f"gs://gresearch/robotics/{ds_info.name}/{ds_info.version}"}]
+        level_of_support = 4
+    except:
+        version = "0.0.0"
+        download = []
+        level_of_support = 0
+        dataset_name = row["Dataset"].replace(" ", "_").lower()
     number_of_trajectories = [{split_name: ds_info.splits[split_name].num_examples} for split_name in ds_info.splits]
+    
+    tags = ["Open-X-Embodiment"]
+    if row["Robot Morphology"] and str(row["Robot Morphology"]) != "nan":
+        tags.append(row["Robot Morphology"])
+    if row["Data Collect Method"] and str(row["Data Collect Method"]) != "nan":
+        tags.append(row["Data Collect Method"])
+    if row["Scene Type"] and str(row["Scene Type"]) != "nan":
+        for tag in row["Scene Type"].strip().split(","):
+            if tag == 'Kitchen (also toy kitchen)':
+                tag = 'Kitchen'
+            tags.append("Scene:" + tag.strip())
+    print(tags)
+    
     config = Config(
-        dataset_name=ds_info.name,
-        version=str(ds_info.version),
-        description=ds_info.description,
-        tag=["open-x", "manipulation", "single-arm", "parallel-jaw-gripper"],
-        citation=ds_info.citation,
-        download=[{"source": "google_bucket", "link": f"gs://gresearch/robotics/{ds_info.name}/{ds_info.version}"}],
+        dataset_name= row['Dataset'],
+        version= version,
+        description= row['Description'],
+        tag=tags,
+        citation=row['Citation'],
+        download=download,
         curation=[{"open_x_embodiment": True}],
         number_of_trajectories= number_of_trajectories,
-        size_in_gb= float(ds_info.dataset_size / 1e9),
-        intended_level_of_support=4,
+        size_in_gb= row["File Size (GB)"],
+        level_of_support=level_of_support,
         copyright="Copyright",
         schema=[
             {"TODO": "schema_todo"}
         ],
         link="TODO",
     )
-    return config
 
-for dataset in DATASETS:
-    b = tfds.builder_from_directory(builder_dir=dataset2path(dataset))
-    info = b.info
-    config = from_open_x_to_config(info)
     if config.validate():
-        print(f"Config for {dataset} is valid")
+        print(f"Config for {dataset_name} is valid")       
+        yaml_output = config.to_yaml()
+        with open(f"./datasets/{dataset_name}.yaml", "w") as f:
+            f.write(yaml_output)
     else:
-        print(f"Config for {dataset} is invalid")
-    
-    yaml_output = config.to_yaml()
-    with open(f"../datasets/{dataset}.yaml", "w") as f:
-        f.write(yaml_output)
+        print(f"Config for {dataset_name} is invalid")
+        print(config)
+
